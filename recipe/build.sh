84752ef57769a31b15de71d7eefb8ab985a34047
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2020-09-08 08:56:35 +0200

    debug libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+echo "DEBUG: libtool" && libtool --version
 ./configure --prefix=${PREFIX}
 make -j${CPU_COUNT}
+echo "DEBUG: config.log" && cat config.log
 make check || (cat tests/test-suite.log && echo "ERROR: make check failed, see above" && exit 1)
 make install
