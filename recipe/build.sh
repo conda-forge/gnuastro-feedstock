38f871396da7f978ca6722fd972c33a3a09f6601
Sebastian Luna-Valero <sebastian.luna.valero@gmail.com>
2023-08-22 14:08:36 +0200

    update build.sh to manually extract source code

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./bootstrapped/build-aux
+
+echo "DEBUG: manually untarring of source code:"
+cd $SRC_DIR
+tar -xf gnuastro-0.20.60-d994.tar.lz
+cp gnuastro-0.20.60-d994/COPYING .
+cd gnuastro-0.20.60-d994
+
+# https://conda-forge.org/docs/maintainer/knowledge_base.html#cross-compilation
+cp $BUILD_PREFIX/share/gnuconfig/config.* .
 
 echo "DEBUG: libtool" && libtool --version
 ./configure --prefix=${PREFIX}
